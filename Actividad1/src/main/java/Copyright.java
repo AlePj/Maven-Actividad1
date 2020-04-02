b603e129ee025468c2a5b39b50e279b6140f0918
Alejandro PJ <ale.pj01@hotmail.com>
2020-04-02 19:43:22 +0200

    version2.0

--- Actividad1/src/main/java/Copyright.java
+++ Actividad1/src/main/java/Copyright.java
@@ -1,6 +1,6 @@
 public class Copyright {
     private String text="Copyright (C) 1883 Alejandro Paniagua Jurado - All Rights Reserved You may use, distribute and modify this code under the terms of the XYZ license, which unfortunately won't be written for another century. You should have received a copy of the XYZ license with this file. If not, please visit : http://www.thomas-edison.com";
-;    public Copyright(String text) {
+;    public Copyright() {
         this.text =text;
     }
 
